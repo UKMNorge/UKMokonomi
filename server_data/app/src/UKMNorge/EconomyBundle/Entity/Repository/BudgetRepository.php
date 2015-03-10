5e0e7989f903dc6b80b1c943737fb5f13659f61f
Marius Mandal <mariusmandal@gmail.com>
2015-03-10 12:18:46 +0100

    Sorter formål etter kode, navn ASC

--- server_data/app/src/UKMNorge/EconomyBundle/Entity/Repository/BudgetRepository.php
+++ server_data/app/src/UKMNorge/EconomyBundle/Entity/Repository/BudgetRepository.php
@@ -12,4 +12,8 @@ use Doctrine\ORM\EntityRepository;
  */
 class BudgetRepository extends EntityRepository
 {
+	public function findAll()
+    {
+        return $this->findBy(array(), array('code' => 'ASC','name' => 'ASC'));
+    }
 }
